89a00e87ebad097f24c3699518dfd3c7a274788b
Eric Denman <edenman@squareup.com>
2012-09-12 15:56:54 -0700

    Fix checkstyle issues

--- src/main/java/com/squareup/squash/SquashEntry.java
+++ src/main/java/com/squareup/squash/SquashEntry.java
@@ -10,10 +10,10 @@ import java.util.Map;
 
 /** An exception to be tracked in Squash (the replacement for Hoptoad, because Hoptoad is lame). */
 // All of the fields are only used for gson serialization, and so can't be made local or removed.
-@SuppressWarnings({"FieldCanBeLocal", "UnusedDeclaration"})
+@SuppressWarnings({ "FieldCanBeLocal", "UnusedDeclaration" })
 public class SquashEntry {
   private static final String DATE_RFC_2822 = "EEE, dd MMM yyyy HH:mm:ss Z";
-  private static final ThreadLocal<DateFormat> dateFormatThreadLocal =
+  private static final ThreadLocal<DateFormat> DATE_FORMAT_THREAD_LOCAL =
       new ThreadLocal<DateFormat>();
 
   // Things that do not change per entry but should still be gson'd.
@@ -55,10 +55,10 @@ public class SquashEntry {
     this.message = error == null ? null : error.getMessage();
     this.api_key = apiKey;
     this.user_id = userId;
-    DateFormat dateFormat = dateFormatThreadLocal.get();
+    DateFormat dateFormat = DATE_FORMAT_THREAD_LOCAL.get();
     if (dateFormat == null) {
       dateFormat = new SimpleDateFormat(DATE_RFC_2822);
-      dateFormatThreadLocal.set(dateFormat);
+      DATE_FORMAT_THREAD_LOCAL.set(dateFormat);
     }
     this.occurred_at = dateFormat.format(new Date());
   }
