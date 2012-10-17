5c19078e7d4e8f8825f88bbfdc853f7ee69975f4
Tim Morgan <tim@squareup.com>
2012-10-17 14:07:46 -0700

    Initial commit

---
+++ src/main/java/com/squareup/squash/SquashBacktrace.java
@@ -0,0 +1,117 @@
+// Copyright 2012 Square Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+package com.squareup.squash;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/** Creates the Squash stacktrace format for serialization by gson. */
+public final class SquashBacktrace {
+
+  // This lets Squash know that the stacktrace is java-style and can contain obfuscated classes.
+  public static final String JAVA_PREFIX = "_JAVA_";
+
+  private SquashBacktrace() {
+    // Should not be instantiated: this is a utility class.
+  }
+
+  public static List<List<Object>> getBacktraces(Throwable error) {
+    if (error == null) {
+      return null;
+    }
+    final ArrayList<List<Object>> threadList = new ArrayList<List<Object>>();
+    final ArrayList<Object> currentThread = new ArrayList<Object>();
+    currentThread.add(Thread.currentThread().getName());
+    currentThread.add(true);
+    currentThread.add(getStacktraceArray(error));
+    threadList.add(currentThread);
+    return threadList;
+  }
+
+  private static List<List<Object>> getStacktraceArray(Throwable error) {
+    List<List<Object>> stackElems = new ArrayList<List<Object>>();
+    for (StackTraceElement element : error.getStackTrace()) {
+      List<Object> elementList = new ArrayList<Object>();
+      elementList.add(JAVA_PREFIX);
+      elementList.add(element.getFileName());
+      elementList.add(element.getLineNumber());
+      elementList.add(element.getMethodName());
+      elementList.add(element.getClassName());
+      stackElems.add(elementList);
+    }
+    return stackElems;
+  }
+
+  public static Map<String, Object> getIvars(Throwable error) {
+    if (error == null) {
+      return null;
+    }
+    Map<String, Object> ivars = new HashMap<String, Object>();
+    final Field[] fields = error.getClass().getDeclaredFields();
+    for (Field field : fields) {
+      try {
+        if (!Modifier.isStatic(field.getModifiers()) // Ignore static fields.
+            && !field.getName().startsWith("CGLIB")) { // Ignore mockito stuff in tests.
+          if (!field.isAccessible()) {
+            field.setAccessible(true);
+          }
+          Object val = field.get(error);
+          ivars.put(field.getName(), val);
+        }
+      } catch (IllegalAccessException e) {
+        ivars.put(field.getName(), "Exception accessing field: " + e);
+      }
+    }
+    return ivars;
+  }
+
+  /**
+   * Recursive method that follows the "cause" exceptions all the way down the stack, adding them to
+   * the passed-in list.
+   */
+  public static void populateNestedExceptions(List<NestedException> nestedExceptions,
+      Throwable error) {
+    // Only keep processing if the "cause" exception is set and != the "parent" exception.
+    if (error == null || error.getCause() == null || error.getCause() == error) {
+      return;
+    }
+    final Throwable cause = error.getCause();
+    NestedException doc = new NestedException(cause.getClass().getName(), cause.getMessage(),
+        getBacktraces(cause), getIvars(cause));
+    nestedExceptions.add(doc);
+    // Exceptions all the way down!
+    populateNestedExceptions(nestedExceptions, cause);
+  }
+
+  /** Wrapper object for nested exceptions. */
+  public static class NestedException {
+    final String class_name;
+    final String message;
+    final List<List<Object>> backtraces;
+    final Map<String, Object> ivars;
+
+    public NestedException(String className, String message, List<List<Object>> backtraces,
+        Map<String, Object> ivars) {
+      this.class_name = className;
+      this.message = message;
+      this.backtraces = backtraces;
+      this.ivars = ivars;
+    }
+  }
+}
---
+++ src/main/java/com/squareup/squash/SquashEntry.java
@@ -0,0 +1,76 @@
+// Copyright 2012 Square Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+package com.squareup.squash;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/** An exception to be tracked in Squash (the replacement for Hoptoad, because Hoptoad is lame). */
+// All of the fields are only used for gson serialization, and so can't be made local or removed.
+@SuppressWarnings({ "FieldCanBeLocal", "UnusedDeclaration" })
+public class SquashEntry {
+  private static final String DATE_RFC_2822 = "EEE, dd MMM yyyy HH:mm:ss Z";
+  private static final ThreadLocal<DateFormat> DATE_FORMAT_THREAD_LOCAL = new ThreadLocal<DateFormat>() {
+    @Override protected DateFormat initialValue() {
+      return new SimpleDateFormat(DATE_RFC_2822);
+    }
+  };
+
+  // Things that do not change per entry but should still be gson'd.
+  private final String client;
+
+  // Things that change per entry.
+  private final String api_key;
+  private final String environment;
+  private final String endpoint;
+  private final String user_id;
+  private final String version;
+  private final String revision;
+  private final String build;
+  private final String occurred_at;
+
+  // Used in tests.
+  final List<List<Object>> backtraces;
+  final Map<String, Object> ivars;
+  final List<SquashBacktrace.NestedException> parent_exceptions;
+  final String class_name;
+  final String message;
+  final String log_message;
+
+  public SquashEntry(String client, String apiKey, String message, Throwable error,
+      String appVersion, int versionCode, String buildSha, String deviceId, String endpoint,
+      String userId, String environment) {
+    this.client = client;
+    this.log_message = message;
+    this.version = appVersion;
+    this.revision = buildSha;
+    this.build = "" + versionCode;
+    this.environment = environment;
+    this.endpoint = endpoint;
+    this.backtraces = SquashBacktrace.getBacktraces(error);
+    this.parent_exceptions = new ArrayList<SquashBacktrace.NestedException>();
+    SquashBacktrace.populateNestedExceptions(parent_exceptions, error);
+    this.ivars = SquashBacktrace.getIvars(error);
+    this.class_name = error == null ? null : error.getClass().getName();
+    this.message = error == null ? null : error.getMessage();
+    this.api_key = apiKey;
+    this.user_id = userId;
+    this.occurred_at = DATE_FORMAT_THREAD_LOCAL.get().format(new Date());
+  }
+}
