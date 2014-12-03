25178c26ee60d4c6519b1ad495f35f5e1c6b0ef4
Ivan Sim <ivans@activestate.com>
2014-12-02 21:29:01 -0800

    Unit tests to cover the correctness of 'SquashException' instance variables. This includes:
    1. The name of the exception object.
    2. The 'faulted' boolean.
    3. The backtrace.

--- src/test/java/com/squareup/squash/SquashBacktrace_GetBacktracesTest.java
+++ src/test/java/com/squareup/squash/SquashBacktrace_GetBacktracesTest.java
@@ -14,11 +14,12 @@
 
 package com.squareup.squash;
 
+import java.util.Date;
 import org.junit.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
 
-public class SquashBacktraceTest{
+public class SquashBacktrace_GetBacktracesTest{
 
   @Test public void testGetBacktraces_WhenErrorIsNull_ReturnsNull(){
     assertThat(SquashBacktrace.getBacktraces(null)).isNull();
@@ -36,6 +37,40 @@ public class SquashBacktraceTest{
 
   @Test public void testGetBacktraces_WhenGivenThrowable_ReturnsSquashException(){
     assertThat(SquashBacktrace.getBacktraces(new Throwable()).get(0).getClass()).isEqualTo(SquashBacktrace.SquashException.class);
+  }
+
+  @Test public void testGetBacktraces_WhenGivenThrowable_SquashExceptionIsFaulted(){
+    assertThat(SquashBacktrace.getBacktraces(new Throwable()).get(0).faulted).isEqualTo(true);
+  }
+
+  @Test public void testGetBacktraces_WhenGivenThrowable_SquashExceptionNameIsEqlToCurrentThreadName(){
+    String expectedName = this.getClass().getName() + "_UnitTestThread";
+    Thread.currentThread().setName(expectedName);
+    assertThat(SquashBacktrace.getBacktraces(new Throwable()).get(0).name).isEqualTo(expectedName);
+  }
+
+  @Test public void testGetBacktraces_WhenGivenThrowable_SquashExceptionBacktraceIsNotEmpty(){
+    assertThat(SquashBacktrace.getBacktraces(new Throwable()).get(0).backtrace).isNotNull();
+    assertThat(SquashBacktrace.getBacktraces(new Throwable()).get(0).backtrace.size()).isNotEqualTo(0);
+  }
+
+  @Test public void testGetBacktraces_WhenGivenThrowableWithStacktrace_StacktraceIsTransformedToBacktrace(){
+    int expectedTraceSize = 10;
+    StackTraceElement[] expectedStacktrace = new StackTraceElement[expectedTraceSize];
+    for(int i = 0; i < expectedTraceSize; i++)
+      expectedStacktrace[i] = new StackTraceElement(this.getClass().getName(), "aUnitTestMethod()", "UnitTestFile.java", i);
+    Throwable throwable =  new Throwable();
+    throwable.setStackTrace(expectedStacktrace);
+
+    assertThat(SquashBacktrace.getBacktraces(throwable).get(0).backtrace).hasSize(expectedTraceSize);
+    
+    int lineNumber = 0;
+    for(SquashBacktrace.StackElement stackElement : SquashBacktrace.getBacktraces(throwable).get(0).backtrace) {
+      assertThat(stackElement.class_name).isEqualTo(this.getClass().getName());
+      assertThat(stackElement.symbol).isEqualTo("aUnitTestMethod()");
+      assertThat(stackElement.file).isEqualTo("UnitTestFile.java");
+      assertThat(stackElement.line).isEqualTo(lineNumber++);
+    }
   }
 }
 
