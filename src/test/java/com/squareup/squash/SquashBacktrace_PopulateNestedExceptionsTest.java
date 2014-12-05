dcf52b90134a0a26b2ba4f41c7ef7fc65f4d4df5
Ivan Sim <ivans@activestate.com>
2014-12-04 23:36:57 -0800

    Recursive method to generate N-level deep nested exceptions.

--- src/test/java/com/squareup/squash/SquashBacktrace_PopulateNestedExceptionsTest.java
+++ src/test/java/com/squareup/squash/SquashBacktrace_PopulateNestedExceptionsTest.java
@@ -11,17 +11,25 @@
 // //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // //    See the License for the specific language governing permissions and
 // //    limitations under the License.
-
 package com.squareup.squash;
 
 import java.util.List;
 import java.util.ArrayList;
 
 import org.junit.Test;
+import org.junit.Before;
 
 import static org.fest.assertions.Assertions.assertThat; 
 
 public class SquashBacktrace_PopulateNestedExceptionsTest{
+
+  private String expectedClassName;
+  private Throwable parent;
+
+  @Before public void setUp(){
+    expectedClassName = Throwable.class.getName();
+    parent = new Throwable(); 
+  }
 
   @Test public void populateNestedException_WhenGivenNullThrowable_NestedExceptionsRemainsUnchanged(){
     List<SquashBacktrace.NestedException> emptyList = new ArrayList<SquashBacktrace.NestedException>();
@@ -30,17 +38,60 @@ public class SquashBacktrace_PopulateNestedExceptionsTest{
   }
 
   @Test public void populateNestedException_WhenGivenThrowableWithOneNestedException_ReturnsItsNestedException() {
-    String expectedClassName = Throwable.class.getName();
-    String expectedExceptionMessage = "Nested Exception";
-    Throwable expectedCause = new Throwable(expectedExceptionMessage);
-    Throwable parent = new Throwable(expectedCause);
-
+    parent.initCause(nestedThrowablesWithDepths(1, null));
     List<SquashBacktrace.NestedException> actualNestedExceptions = new ArrayList<SquashBacktrace.NestedException>();
     SquashBacktrace.populateNestedExceptions(actualNestedExceptions, parent);
-    assertNestedExceptionAttributes(actualNestedExceptions.get(0), expectedExceptionMessage, expectedClassName);
+    assertNestedExceptions(actualNestedExceptions);
   }
 
-  private void assertNestedExceptionAttributes(SquashBacktrace.NestedException fixture, String expectedExceptionMessage, String expectedClassName){
+  @Test public void populateNestedException_WhenGivenThrowableWithFiveNestedException_ReturnsAllNestedExceptions() {
+    parent.initCause(nestedThrowablesWithDepths(5, null));
+    List<SquashBacktrace.NestedException> actualNestedExceptions = new ArrayList<SquashBacktrace.NestedException>();
+    SquashBacktrace.populateNestedExceptions(actualNestedExceptions, parent);
+    assertNestedExceptions(actualNestedExceptions);
+  }
+
+  @Test public void populateNestedException_WhenGivenThrowableWithTenNestedException_ReturnsAllNestedExceptions() {
+    parent.initCause(nestedThrowablesWithDepths(10, null));
+    List<SquashBacktrace.NestedException> actualNestedExceptions = new ArrayList<SquashBacktrace.NestedException>();
+    SquashBacktrace.populateNestedExceptions(actualNestedExceptions, parent);
+    assertNestedExceptions(actualNestedExceptions);
+  }
+
+  @Test public void populateNestedException_WhenGivenThrowableWithThirtyNestedException_ReturnsAllNestedExceptions() {
+    parent.initCause(nestedThrowablesWithDepths(30, null));
+    List<SquashBacktrace.NestedException> actualNestedExceptions = new ArrayList<SquashBacktrace.NestedException>();
+    SquashBacktrace.populateNestedExceptions(actualNestedExceptions, parent);
+    assertNestedExceptions(actualNestedExceptions);
+  }
+
+  @Test public void populateNestedException_WhenGivenThrowableWithSixtyNestedException_ReturnsAllNestedExceptions() {
+    parent.initCause(nestedThrowablesWithDepths(60, null));
+    List<SquashBacktrace.NestedException> actualNestedExceptions = new ArrayList<SquashBacktrace.NestedException>();
+    SquashBacktrace.populateNestedExceptions(actualNestedExceptions, parent);
+    assertNestedExceptions(actualNestedExceptions);
+  }
+
+  private Throwable nestedThrowablesWithDepths(int depth, Throwable cause) {
+    if(depth == 0)
+      return cause;
+
+    String message = "Level[" + depth + "]: This Is A Nested Exception";
+    Throwable throwable = new Throwable(message);
+    if (cause != null)
+      throwable.initCause(cause);
+    return nestedThrowablesWithDepths(depth - 1, throwable);
+  }
+
+  private void assertNestedExceptions(List<SquashBacktrace.NestedException> nestedExceptions){
+    for(int i = 0; i < nestedExceptions.size(); i++) {
+      int depth = i + 1;
+      String expectedMessage = "Level[" + depth + "]: This Is A Nested Exception";
+      assertNestedExceptionAttributes(nestedExceptions.get(i), expectedMessage);
+    }
+  }
+
+  private void assertNestedExceptionAttributes(SquashBacktrace.NestedException fixture, String expectedExceptionMessage){
     assertThat(fixture).isNotNull();
     assertThat(fixture.class_name).isEqualTo(expectedClassName);
     assertThat(fixture.message).isEqualTo(expectedExceptionMessage);
